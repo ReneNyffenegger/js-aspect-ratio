49cef9f30a7c1134c699a1bd7865f9b199216a7a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-10-14 14:31:03 +0200

    Initial checkin

---
+++ aspect_ratio.js
@@ -0,0 +1,75 @@
+"use strict";
+
+function viewport() {
+//
+// Found at:
+//    https://andylangton.co.uk/blog/development/get-viewportwindow-size-width-and-height-javascript
+//
+
+  var e = window,
+      a = 'inner';
+
+  if ( !('innerWidth' in window ) ) {
+    a = ' client';
+    e =   document.documentElement || document.body;
+  }
+
+  return { width : e[ a+'Width' ],
+           height: e[ a+'Height' ]
+  }
+}
+
+
+
+var tq84 = tq84 || {};
+
+tq84.aspect_ratio = {};
+
+
+tq84.aspect_ratio.init = function (
+    canvas,
+    original_w,
+    original_h,
+    opts
+) {
+
+  self.canvas       = canvas;
+  self.original_w   = original_w;
+  self.original_h   = original_h;
+  self.aspect_ratio = original_h / original_w;
+
+  self.resized();
+
+}
+
+tq84.aspect_ratio.resized = function() {
+
+  var vw=viewport();
+
+  var vw_aspect_ratio = vw.height / vw.width;
+
+  var scale;
+  if (vw_aspect_ratio < self.aspect_ratio) {
+  //
+  //  The viewport is wider than the canvas
+  //
+
+     scale = vw.height / self.original_h;
+      
+  }
+  else {
+  //
+  //  The viewport is higher than the canvas
+  //
+     scale = vw.width / self.original_w;
+
+  }
+
+  let gap_h = (vw.height - self.original_h) / 2;
+  let gap_w = (vw.width  - self.original_w) / 2;
+  self.canvas.style.left = gap_w + 'px';
+  self.canvas.style.top  = gap_h + 'px'; 
+  self.canvas.style.transform='scale('+(scale*1)+')';
+
+
+}
